fdc77c5331b64c789a579b789b5e9296014f230f
随随意 <suisuiyi@192.168.43.119>
2016-06-08 14:06:50 +0800

    can drag

--- DragCollectionView/ViewController.swift
+++ DragCollectionView/ViewController.swift
@@ -8,17 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    private var longPressGestrure: UILongPressGestureRecognizer!
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
+    longPressGestrure = UILongPressGestureRecognizer(target: self, action: "handleLongGesture:")
         
-        print("test")
+    collectionView.addGestureRecognizer(longPressGestrure)
+            }
+    func handleLongGesture(gesture: UILongPressGestureRecognizer) {
+        switch gesture.state {
+        case UIGestureRecognizerState.Began:
+            guard let selectedIndexPath = self.collectionView.indexPathForItemAtPoint(gesture.locationInView(collectionView)) else {
+                break
+            }
+            collectionView.beginInteractiveMovementForItemAtIndexPath(selectedIndexPath)
+        case UIGestureRecognizerState.Changed:
+            collectionView.updateInteractiveMovementTargetPosition(gesture.locationInView(gesture.view!))
+        case UIGestureRecognizerState.Ended:
+            collectionView.endInteractiveMovement()
+        default: collectionView.cancelInteractiveMovement()
+        }
+        
+        
+    
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("haha", forIndexPath: indexPath)
+        if let label = cell.contentView.subviews.first as? UILabel {
+            label.text = indexPath.row.description
+        }
+        
+        return cell
+
+    }
+    //移动代码
+    func collectionView(collectionView: UICollectionView, canMoveItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    func collectionView(collectionView: UICollectionView, moveItemAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+        
+    }
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        print("选中了\(indexPath.row)")
     }
 
 
